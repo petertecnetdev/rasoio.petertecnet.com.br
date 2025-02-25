f2cc1edf26757c93bb6d68aa6e6bc598ae38e524
petertecnetdev <petertecnet@gmail.com>
2025-02-25 13:02:58 -0300

    all done for new vps

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const apiBaseUrl = "http://api.petertecnet.com.br/api";
-const storageUrl = "http://api.petertecnet.com.br/public";
+const apiBaseUrl = "https://api.petertecnet.com.br/api";
+const storageUrl = "https://api.petertecnet.com.br/public";
 export { apiBaseUrl, storageUrl };
