1f4b78d545a1d19a8c8a82d8bbeaa016d974141a
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 12:54:38 -0300

    fix: use Rasoio application id

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const apiBaseUrl = "https://api.petertecnet.com.br/api";
 const storageUrl = "https://api.petertecnet.com.br/storage/";
 
-const appId =  2;
-const linkApp ="https://rasoio.petertecnet.com.br";
+const appId = 1;
+const linkApp = "https://rasoio.petertecnet.com.br";
+
 export { apiBaseUrl, storageUrl, appId, linkApp };
