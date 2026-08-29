d57d38a4de69a59c669035af6ae0dbd850567e9b
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 21:21:37 -0300

    feat: show Peter Tecnet signature globally

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AppErrorBoundary from "./components/AppErrorBoundary";
+import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <AppErrorBoundary>
       <App />
+      <PeterTecnetSignature />
     </AppErrorBoundary>
   </React.StrictMode>
 );
