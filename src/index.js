5d70a3b9045c9659390df612664625de8aaf4fa9
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:44:23 -0300

    Sign direct Rasoio axios requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App";
 import AppErrorBoundary from "./components/AppErrorBoundary";
 import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+
+axios.defaults.headers.common["X-Peter-App"] = "rasoio";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
