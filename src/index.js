a4025c345f3629b2207677d72e83af37760f435e
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:45:09 -0300

    feat: bootstrap Peter Account before Rasoio

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import App from "./App";
 import AppErrorBoundary from "./components/AppErrorBoundary";
 import PeterTecnetSignature from "./components/PeterTecnetSignature";
+import PeterAccountGateway from "./components/PeterAccountGateway";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./styles/appointment-step1-compact.css";
@@ -20,9 +21,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AppErrorBoundary>
-      <App />
-      <PeterTecnetSignature />
-    </AppErrorBoundary>
+    <PeterAccountGateway apiBaseUrl={apiBaseUrl} appSlug={appSlug}>
+      <AppErrorBoundary>
+        <App />
+        <PeterTecnetSignature />
+      </AppErrorBoundary>
+    </PeterAccountGateway>
   </React.StrictMode>
 );
