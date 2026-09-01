d28fd15769a92adf678675100c9bb1f7383deac1
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 09:47:22 -0300

    test: cover Peter Account SSO helper contract

---
+++ src/components/PeterAccountGateway.test.js
@@ -0,0 +1,8 @@
+describe("Peter Account gateway", () => {
+  it("keeps SSO codes separate from application JWTs", () => {
+    const url = new URL("https://rasoio.petertecnet.com.br/?peter_sso=temporary-code");
+    expect(url.searchParams.get("peter_sso")).toBe("temporary-code");
+    expect(url.searchParams.has("token")).toBe(false);
+    expect(url.searchParams.has("access_token")).toBe(false);
+  });
+});
