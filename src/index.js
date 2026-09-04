67b9eb461349daefae7f0dae66aa0fa91f926fab
Peter Tecnet <petertecnet@gmail.com>
2026-09-04 17:37:26 -0300

    refactor: use shared telemetry bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { startTelemetry } from "./telemetry";
 import { apiBaseUrl, appId, appSlug } from "./config";
 import { installGlobalImageFallbacks } from "./utils/imageFallback";
 import { installPasswordFieldEnhancer } from "./utils/passwordFieldEnhancer";
@@ -16,7 +15,6 @@ import "./styles/appointment-wizard-responsive.css";
 
 installGlobalImageFallbacks();
 installPasswordFieldEnhancer();
-startTelemetry({ apiBaseUrl, appSlug, appId });
 
 axios.defaults.headers.common["X-Peter-App"] = appSlug;
 axios.defaults.headers.common["X-App-ID"] = String(appId);
